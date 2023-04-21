1079e57c7468e8317399568ebf61a8a92dc9d00c
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-04-21 20:16:46 +0300

    Seperate modules for fdd

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
+  imports: [BrowserModule, CoreModule, FeaturesModule, SharedModule],
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ frontend/src/app/core/core.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+import { RoutingModule } from './routing/routing.module';
+
+@NgModule({
+  exports: [RoutingModule],
+})
+export class CoreModule {}
